dd901a9d670bfac875b69d5c4847b5f416dbf1b9
Albert <albertwang@AlbertMBP.lan>
2023-09-06 02:17:48 +0800

    refactor: add config and scripts dir

---
+++ scripts/pre-softlink.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function create_softlink_neovim {
+	local origin_neovim_path="${HOME}/.config/nvim"
+	local git_root=$(git rev-parse --show-toplevel 2>/dev/null)
+	local config_neovim_path="${git_root}/config"
+
+	if [[ ! -f "${origin_neovim_path}" ]] && [[ ! -L "${config_neovim_path}" ]]; then
+		ln -s "${config_neovim_path}" "${origin_neovim_path}"
+		printf '%s\n' "create link to ${origin_neovim_path}."
+	else
+		printf '%s\n' "file ${origin_neovim_path} already existed."
+	fi
+}
+
+create_softlink_neovim
